93d962b0846528d6c47b25916d8736e7bc1bc754
caren580 <chumacaren69@gmail.com>
2025-07-04 12:49:34 +0300

    wrap up project structure with all core components and assets

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Destinations from "./pages/Destination";
+import TripTypes from './pages/TripTypes';
+import ContactPage from './pages/Contact';
+import Footer from './components/Footer';
+
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/destinations" element={<Destinations />} />
+          <Route path="/triptypes" element={<TripTypes />} />
+          <Route path="/contactus" element={<ContactPage />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
+}
+export default App;
