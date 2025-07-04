d869e3be5c281a2493507093f25e51285cf84aab
caren580 <chumacaren69@gmail.com>
2025-07-04 11:17:45 +0300

    added responsive Header with navigation links and logo icon

---
+++ src/components/Header.tsx
@@ -0,0 +1,44 @@
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Stack,
+  Button,
+} from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { BsGlobeEuropeAfrica } from "react-icons/bs";
+
+function Header() {
+  return (
+    <AppBar position="static" sx={{ backgroundColor: "#090040" }}>
+      <Toolbar>
+        <IconButton size="large" edge="start" color="inherit">
+          <BsGlobeEuropeAfrica />
+        </IconButton>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          Zaph Tours
+        </Typography>
+
+        {/* NavLinks */}
+        <Stack direction="row" spacing={2}>
+          <Button color="inherit"  component={RouterLink} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={RouterLink} to="/destinations">
+            Destination
+          </Button>
+          <Button color="inherit" component={RouterLink} to="/triptypes">
+            Trip types
+          </Button>
+          <Button color="inherit" component={RouterLink} to="/contactus">
+            Contact us
+          </Button>
+          
+        </Stack>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header;
